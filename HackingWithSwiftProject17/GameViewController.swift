f0dbef72cf95788b96f8b76de4377ef764858878
pedro-c-aquino <pedro.aquino@foursys.com.br>
2022-11-28 10:05:42 -0800

    Project 17 finished

--- HackingWithSwiftProject17/GameViewController.swift
+++ HackingWithSwiftProject17/GameViewController.swift
@@ -13,7 +13,6 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
@@ -29,6 +28,10 @@ class GameViewController: UIViewController {
             view.showsFPS = true
             view.showsNodeCount = true
         }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
